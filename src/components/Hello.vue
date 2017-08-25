dbba4ba1b41f1869e2515d6c3def9c141bee9fcc
Thanh Phan <thanh.phanduy@pixta.co.jp>
2017-08-25 15:10:57 +0700

    Added SCSS loader

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -32,7 +32,13 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+.hello {
+  h1 {
+    font-weight: bold;
+  }
+}
+
 h1, h2 {
   font-weight: normal;
 }
